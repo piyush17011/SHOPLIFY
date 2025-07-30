ef1832715bc7ba9d8a07909e6f493a47e5047b78
piyush17011 <piyushbhalwalkar01@gmail.com>
2025-07-30 11:33:34 +0530

    Initial commit

---
+++ backend/dbConnection.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongoURL = process.env.MONGO_URL;
+const connection= async() =>{
+    try{
+        const connection  = await mongoose.connect(mongoURL);
+    }       
+    catch(err){
+        console.log(err);
+    }
+}
+module.exports = connection;
